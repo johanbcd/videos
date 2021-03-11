8455eb3f41eba00282ee92406c157170f45571a2
Johan Baracaldo <johanbcd@gmail.com>
2021-03-11 18:32:25 +0100

    Primer commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+export default function App() {
+    return (
+        <div>
+            <h1>hola pruebafffff</h1>
+        </div>
+    )
+}
